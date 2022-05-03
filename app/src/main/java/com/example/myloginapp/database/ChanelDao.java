e2f094da71db4beeeff5a004f073b8ff34d98b9f
Nam Nhu <nhn010195@gmail.com>
2022-05-03 18:43:52 +0700

    Add history and  Swiperefreshlayout

---
+++ app/src/main/java/com/example/myloginapp/database/ChanelDao.java
@@ -0,0 +1,22 @@
+package com.example.myloginapp.database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.myloginapp.Chanel;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+@Dao
+public interface ChanelDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void InsertChanel(Chanel chanel);
+
+    @Query("SELECT * FROM chanel")
+    List<Chanel> getListChanel();
+}
--- app/src/main/java/com/example/myloginapp/fragment/HistoryFragment.java
+++ app/src/main/java/com/example/myloginapp/fragment/HistoryFragment.java
@@ -4,63 +4,80 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.example.myloginapp.Chanel;
+import com.example.myloginapp.ChanelAdapter;
 import com.example.myloginapp.R;
-import com.example.myloginapp.Cartoon;
 import com.example.myloginapp.CartoonAdapter;
+import com.example.myloginapp.database.ChanelDatabase;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-public class HistoryFragment extends Fragment {
-    private RecyclerView rcvUser ;
-    private CartoonAdapter mUserAdapter;
+public class HistoryFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+    ChanelAdapter chanelAdapter;
+    RecyclerView recyclerView;
+    private List<Chanel> mChanelsList;
+    private SwipeRefreshLayout swipeRefreshLayout;
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        rcvUser = view.findViewById(R.id.rcv_user);
+        swipeRefreshLayout = view.findViewById(R.id.swiperReflayout);
+
+        mChanelsList = new ArrayList<>();
+        recyclerView = view.findViewById(R.id.rcv_user);
 //        getActivity().findViewById(R.id.rcv_user);
-        mUserAdapter = new CartoonAdapter(requireContext());
+        chanelAdapter = new ChanelAdapter(requireContext(),mChanelsList,0);
+        recyclerView.setAdapter(chanelAdapter);
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(requireContext(), 3);
-        rcvUser.setLayoutManager(gridLayoutManager);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        getListChanel();
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(itemDecoration);
+        swipeRefreshLayout.setOnRefreshListener(this);
 
-        mUserAdapter.setData(getListUser());
-        rcvUser.setAdapter(mUserAdapter);
+//        chanelAdapter.addView(getListChanel());
+
+//        ChanelAdapter.
+//        recyclerView.setAdapter(chanelAdapter);
     }
-
-    private List<Cartoon> getListUser() {
-        List<Cartoon> list = new ArrayList<>();
-        list.add(new Cartoon(R.drawable.fim1, "User Name 1"));
-        list.add(new Cartoon(R.drawable.fim2, "User Name 2"));
-        list.add(new Cartoon(R.drawable.fim3, "User Name 3"));
-        list.add(new Cartoon(R.drawable.fim4, "User Name 4"));
-
-        list.add(new Cartoon(R.drawable.fim1, "User Name 1"));
-        list.add(new Cartoon(R.drawable.fim2, "User Name 2"));
-        list.add(new Cartoon(R.drawable.fim3, "User Name 3"));
-        list.add(new Cartoon(R.drawable.fim4, "User Name 4"));
-
-        list.add(new Cartoon(R.drawable.fim1, "User Name 1"));
-        list.add(new Cartoon(R.drawable.fim2, "User Name 2"));
-        list.add(new Cartoon(R.drawable.fim3, "User Name 3"));
-        list.add(new Cartoon(R.drawable.fim4, "User Name 4"));
-
-        return list;
+    private List<Chanel> getListChanel() {
+        mChanelsList.clear();
+        mChanelsList = ChanelDatabase.getInstance(getContext()).chanelDao().getListChanel();
+        return mChanelsList;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+
+    @Override
+    public void onRefresh() {
+        chanelAdapter.clear();
+        chanelAdapter.addView(getListChanel());
+        Handler handler  = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        },3000);
     }
 }
